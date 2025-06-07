cc5032a5223032b2f4f60de1717f16531a2c27ab
Nicholas Russell <nrus795@aucklanduni.ac.nz>
2025-06-07 17:23:40 +1200

    fix: changed erroneous procedure to function in strl
    turns out you can't actually compile things to what they're not declared
    too

--- Corban_stuff/new_code/CruiseControl/cruiseControl.h
+++ Corban_stuff/new_code/CruiseControl/cruiseControl.h
@@ -1,9 +1,9 @@
 #ifndef CRUISECONTROL_DATA_H
 #define CRUISECONTROL_DATA_H
 
-#include <stdbool.h>
+// #include <stdbool.h>
 
-float regulateThrottle(bool isGoingOn, float cruiseSpeed, float vehicleSpeed);
-float saturateThrottle(float throttleIn, bool* saturate);
+float regulateThrottle(boolean isGoingOn, float cruiseSpeed, float vehicleSpeed);
+float saturateThrottle(float throttleIn, boolean* saturate);
 
 #endif
