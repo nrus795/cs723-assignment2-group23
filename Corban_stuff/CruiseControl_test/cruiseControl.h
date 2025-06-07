3a83de81d23ee91ac1687eb9461781aa392c40a6
MKXLII <cell376@aucklanduni.ac.nz>
2025-06-07 21:54:58 +1200

    finally so close but just so far :(

---
+++ Corban_stuff/CruiseControl_test/cruiseControl.h
@@ -0,0 +1,15 @@
+#ifndef CRUISECONTROL_DATA_H
+#define CRUISECONTROL_DATA_H
+
+// #include <csimul.h>
+#include <csimul.h>
+#include <stddef.h>
+// #include <csimul.h>
+#include <stdbool.h>
+// #include <csimul.h>
+// typedef int boolean;
+
+float regulateThrottle(boolean isGoingOn, float cruiseSpeed, float vehicleSpeed);
+float saturateThrottle(float throttleIn, bool* saturate);
+
+#endif
