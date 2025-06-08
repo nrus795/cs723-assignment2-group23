d047f76061c983d15db0325e1a6fcffad6a05d5b
Nicholas Russell <nrus795@aucklanduni.ac.nz>
2025-06-08 13:10:01 +1200

    feat: fix boolean error
    turns out the library i was trying to use was borked. fixed by just
    pulling the typedef out

--- Corban_stuff/CruiseControl_test/cruiseControl.h
+++ Corban_stuff/CruiseControl_test/cruiseControl.h
@@ -1,15 +1,16 @@
 #ifndef CRUISECONTROL_DATA_H
 #define CRUISECONTROL_DATA_H
 
-// #include <csimul.h>
-// #include <csimul.h>
 #include <stddef.h>
-// #include <csimul.h>
 #include <stdbool.h>
 // #include <csimul.h>
+#ifndef BASIC_TYPES_DEFINED
+#define BASIC_TYPES_DEFINED
+typedef int boolean;
+#endif
 // typedef int boolean;
 
 float regulateThrottle(boolean isGoingOn, float cruiseSpeed, float vehicleSpeed);
-float saturateThrottle(float throttleIn, bool* saturate);
+float saturateThrottle(float throttleIn, bool *saturate);
 
 #endif
